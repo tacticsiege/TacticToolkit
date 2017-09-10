17671cd10458ae8ca80f4f7c6f82cb4f6966debc
Ryan Little <TacticSiege@gmail.com>
2017-09-09 21:49:50 -0400

    Stubbed ttk sln

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # TacticToolkit
 Collection of machine learning and natural language processing utilities.  
+
+## Installation
+### Dependencies
+### User installation
+
+## Development
+
+### Links
+- Official repo
+- [Releases]
+- Issue tracker
+
+### Source
+```
+git clone https://github.com/tacticsiege/ttk.git
+```
+
+### Environment setup
+
+### Testing
+
+## Project History
+
+## License
+
+## Help and Support
+
+### Doctumentation
+### Communication
+### Citation
---
+++ csharp/TacticToolkit.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TacticToolkit.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TacticToolkit.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3335d2bd-b729-4ff2-9c45-0297419e8f45")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
