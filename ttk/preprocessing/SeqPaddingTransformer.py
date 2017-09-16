42d4e8c1a385772f7b160dfa302018c42c8912b3
Ryan Little <TacticSiege@gmail.com>
2017-09-16 10:56:44 -0400

    Back at it, padding them seq

--- ttk/preprocessing/SeqPaddingTransformer.py
+++ ttk/preprocessing/SeqPaddingTransformer.py
@@ -1,4 +1,58 @@
 class SeqPaddingTransformer(object):
-    """description of class"""
-    pass
+    """ Transforms a varying length set of sequences by padding with zeros. """
+    def __init__(self):
+        self._fit = False
+        self.max_length = 0
+
+    def fit_transform(self, X):        
+        self.__fit(X)
+        return self.__transform(X)
+           
+
+    def fit(self, X):
+        self.__fit(X)
+        return self
+
+    def transform(self, X):
+        transformed = self.__transform(X)
+        return transformed
+
+    def inverse_transform(self, y):
+        transformed = []
+        for seq in y:
+            i = 0
+            token = seq[i]
+            transformed_seq = []
+            seq_length = len(seq)
+            while type(token) is str:
+                transformed_seq.append(token)
+                i += 1
+                if i < seq_length:
+                    token = seq[i]
+                else:
+                    token = 0.0
+            
+            transformed.append(transformed_seq)
+
+        return transformed
+    
+
+    def __fit(self, X):
+        # find longest sequence in set and save
+        self.max_length = len(max(X, key=len))
+        self._fit = True
+
+    def __transform(self, X):
+        X_padded = []
+        for seq in X:
+            x = []
+            seq_length = len(seq)
+            for i in range(self.max_length):
+                if i < seq_length:
+                    x.append(seq[i])
+                else:
+                    x.append(0.0)
+            X_padded.append(x)
+        return X_padded
+
 
---
+++ ttk/preprocessing/SeqPaddingTransformerTests.py
@@ -0,0 +1,46 @@
+import unittest
+
+from ttk.preprocessing import SeqPaddingTransformer
+
+class Test_SeqPaddingTransformerTests(unittest.TestCase):
+    
+    
+    def setUp(self):
+        self.example_X = [
+            ['Two', 'Words'] ,
+            ['Four', 'Words', 'In', 'This'],
+            ['Three', 'Words', 'now'],
+        ]
+    
+    def test_fit_transform(self):
+        transformer = SeqPaddingTransformer()
+        self.assertFalse(transformer._fit)
+
+        X = transformer.fit_transform(self.example_X)
+        for x in X:
+            self.assertEqual(4, len(x))
+
+        self.assertEqual(['Two', 'Words', 0.0, 0.0], X[0])
+        self.assertEqual(['Four', 'Words', 'In', 'This'], X[1])
+        self.assertEqual(['Three', 'Words', 'now', 0.0], X[2])
+
+        self.assertTrue(transformer._fit)
+
+    def test_inverse_transform(self):
+        transformer = SeqPaddingTransformer()
+
+        # doesn't even need fitting
+        y = [
+            ['Two', 'Words', 0.0, 0.0],
+            ['Four', 'Words', 'In', 'This'],
+            ['Three', 'Words', 'now', 0.0],
+        ]
+
+        tranformed = transformer.inverse_transform(y)
+        self.assertEqual(3, len(tranformed))
+        self.assertEqual(self.example_X[0], tranformed[0])
+        self.assertEqual(self.example_X[1], tranformed[1])
+        self.assertEqual(self.example_X[2], tranformed[2])
+
+if __name__ == '__main__':
+    unittest.main()
